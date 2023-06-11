2a24edf59f0c64909a9adbe901271e2ad1e23cdc
yorkeric <yorkeric@gmail.com>
2023-06-11 16:19:00 -0400

    programming question 4 + refactors

--- chapter4_exercises.py
+++ chapter4_exercises.py
@@ -10,13 +10,13 @@ while keep_going == 'y':
     secondNumber = int(input("Enter a number: "))
     print("The sum of the two numbers is", (firstNumber + secondNumber))
     keep_going = input("Do you want to try again? y or n: ")
-    
+
 # Question 3
 # Add for loop with a range. Use range number 101 equlivant to 0, 101
-# Each loop, print the calculation of the range value times 10 
+# Each loop, print the calculation of the range value times 10
 # Format with , separated instead of new line (default format)
 for number in range(101):
-    print(number * 10, end = ",")
+    print(number * 10, end=",")
 
 
 # Programming Exercises
@@ -25,8 +25,12 @@ for number in range(101):
 # Use the augmented assignment operator to calculate the running total of bugs collected
 # After the loop, print the total bugs collected
 total_bugs_collected = 0
-bugs_collected = 0
 for day in range(1, 6):
-    bugs_collected = int(input("Enter number of bugs collected: "))
-    total_bugs_collected += bugs_collected
+    total_bugs_collected += int(input("Enter number of bugs collected on day " + str(day) + ": "))
 print(f'You collected a total of {total_bugs_collected} bugs.')
+
+## Question 2 - Distance Traveled
+speed = int(input("What is the speed of the vehicle in mph? "))
+hours = int(input("How many hours has it traveled? "))
+for hour in range(1, hours):
+    print(str(hour) + "        " + str((speed * hour)))
