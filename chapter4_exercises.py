c828befee9ed5b355a38aab5adcceee36d6426f3
yorkeric <yorkeric@gmail.com>
2023-06-11 11:15:49 -0400

    initial commit question 2

---
+++ chapter4_exercises.py
@@ -0,0 +1,11 @@
+# Algorithm Workbench
+# Question 2
+# Set iterator with inital value of y and loop until user sets value to n
+# Prompt user for two values and print the sum
+# Prompt user to try again. y will loop again and n will stop loop
+keep_going = 'y'
+while keep_going == 'y':
+    firstNumber = int(input("Enter a number: "))
+    secondNumber = int(input("Enter a number: "))
+    print("The sum of the two numbers is ", (firstNumber + secondNumber))
+    keep_going = input("Do you want to try again? y or n: ")
